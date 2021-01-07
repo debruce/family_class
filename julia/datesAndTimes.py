5795587ea474be701654c147b51095908e88e772
Julia Bruce <julia.v.bruce@gmail.com>
2021-01-07 15:54:31 -0800

    datetime completed

--- julia/datesAndTimes.py
+++ julia/datesAndTimes.py
@@ -19,11 +19,9 @@ elif x == "date":
 elif x == "date distance":
     f_date = input("Enter a date: ")
     l_date = input("Enter a second date: ")
-    #a = date(convert(f_date))
-    #b = date(convert(l_date))
-    #a = datetime.datetime.strptime(f_date, "y%m%d%").date()
-    #b = datetime.datetime.strptime(l_date, "y%m%d%").date()
-    c = b - a
+    a = datetime.datetime.strptime(f_date, "%Y-%m-%d")
+    b = datetime.datetime.strptime(l_date, "%Y-%m-%d")
+    c = abs(b - a)
     print("The number of days between those dates is ", c.days)
 else: 
     print("Try 'weekday', 'time', 'date', or 'date distance'.")
