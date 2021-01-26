eb46180b5d886726e87138b57b7aa599769ca4c0
David Bruce <david.e.bruce@icloud.com>
2021-01-25 18:29:22 -0800

    for danny

---
+++ julia/loop_demos.py
@@ -0,0 +1,21 @@
+
+for i in range(4):
+    print("statement1", i)
+    print("statement2", i)
+print("statement after for loop")
+
+# w = 10
+
+# while w > 0:
+#     print("while statement1", w)
+#     print("while statement2", w)
+#     w -= 2
+# print("statement after while loop")
+
+# for i in range(20):
+#     comparison_value = (i == 10) * 100
+#     print(f"comparison_value={comparison_value}")
+#     if comparison_value:
+#         print('hurray!')
+#     else:
+#         print(i)
