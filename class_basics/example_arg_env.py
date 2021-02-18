8d50049cf44462a78c512ea7c70968fe44b1c94a
David Bruce <david.e.bruce@icloud.com>
2021-02-17 18:56:12 -0800

    add argparse

--- class_basics/example_arg_env.py
+++ class_basics/example_arg_env.py
@@ -2,11 +2,34 @@
 
 import os
 import sys
+import time
+import argparse
 
-for var in os.environ:
-    print(f"environment variable {var:20s} is set to {os.environ[var]}")
+parser = argparse.ArgumentParser()
+parser.add_argument("echo", help="echo the string you use here", type=int)
+parser.add_argument("other", help="other string is set here", type=float)
+parser.add_argument("--verbosity", help="increase output verbosity")
+parser.add_argument("--verbose", help="increase output verbosity",
+                    action="store_true")
 
-print()
 
-for var in sys.argv:
-    print(f"argument variable {var}")
+args = parser.parse_args()
+
+print(f"echo is set to {args.echo}")
+print(f"other is set to {args.other}")
+print(f"verbosity is set to {args.verbosity}")
+print(f"verbose is set to {args.verbose}")
+
+if args.verbosity:
+    print(f"verbosity is assigned and has a value of {args.verbosity}")
+
+# print(f"my pid is {os.getpid()}")
+# print(f"my parent's pid is {os.getppid()}")
+
+# for key,value in os.environ.items():
+#     print(f"environment variable {key:20s} is set to {value}")
+
+# print()
+
+# for var in sys.argv:
+#     print(f"argument variable {var}")
